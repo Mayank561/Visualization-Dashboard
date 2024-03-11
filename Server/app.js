a5953043a5e9f759aa8b6ff040d6a14fbea5fecc
Mayank Gupta <108197241+Mayank561@users.noreply.github.com>
2024-03-11 17:49:21 +0530

    Update app.js

--- Server/app.js
+++ Server/app.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Set 'credentials: true' to pass headers, cookies, etc. to browser/frontend
-app.use(cors());
+app.use(cors({https://visualization-dashboard-five.vercel.app/}));
 
 // Route splitting
 app.use("/api/data", reportRouter);
